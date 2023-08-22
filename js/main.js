c9afbb9e55e25d5095f975970da7f9547411eae8
Victor Molina <molinavitillo@gmail.com>
2023-08-22 12:12:25 -0300

    cambio final

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ class AgregarTurno {
     this.fecha = document.getElementById("fechaInput");
     this.horario = document.getElementById("horaInput");
     this.contenedorTurnos = this.cargarLocalStorage();
+    this.mantenerInfo();
     this.enviarTurnos();
   }
-
   agregarTurno(e) {
     e.preventDefault();
     const DateTime = luxon.DateTime;
@@ -57,6 +57,45 @@ class AgregarTurno {
       this.numCelular.value = "";
     }
   }
+  mantenerInfo() {
+    let valoresAlmacenados = {
+      nombre: localStorage.getItem("valueNombre"),
+      numCelular: localStorage.getItem("valueNumCelular"),
+      motivo: localStorage.getItem("valueMotivo"),
+      fecha: localStorage.getItem("valueFecha"),
+      horario: localStorage.getItem("valueHorario"),
+    };
+    if (valoresAlmacenados.nombre) {
+      this.nombre.value = valoresAlmacenados.nombre;
+    }
+    if (valoresAlmacenados.numCelular) {
+      this.numCelular.value = valoresAlmacenados.numCelular;
+    }
+    if (valoresAlmacenados.motivo) {
+      this.motivo.value = valoresAlmacenados.motivo;
+    }
+    if (valoresAlmacenados.fecha) {
+      this.fecha.value = valoresAlmacenados.fecha;
+    }
+    if (valoresAlmacenados.horario) {
+      this.horario.value = valoresAlmacenados.horario;
+    }
+    this.nombre.addEventListener("input", () => {
+      localStorage.setItem("valueNombre", this.nombre.value);
+    });
+    this.numCelular.addEventListener("input", () => {
+      localStorage.setItem("valueNumCelular", this.numCelular.value);
+    });
+    this.motivo.addEventListener("input", () => {
+      localStorage.setItem("valueMotivo", this.motivo.value);
+    });
+    this.fecha.addEventListener("input", () => {
+      localStorage.setItem("valueFecha", this.fecha.value);
+    });
+    this.horario.addEventListener("input", () => {
+      localStorage.setItem("valueHorario", this.horario.value);
+    });
+  }
 
   swalFireError(mensajeDeError) {
     Swal.fire({
@@ -65,21 +104,23 @@ class AgregarTurno {
       icon: "error",
     });
   }
-
   cargarLocalStorage() {
     const turnos_en_JSON = localStorage.getItem("listaDeTurnos");
     return turnos_en_JSON ? JSON.parse(turnos_en_JSON) : [];
   }
-
   guardarStorage(turnos) {
     let turnos_en_JSON = JSON.stringify(turnos);
     localStorage.setItem("listaDeTurnos", turnos_en_JSON);
   }
-
   enviarTurnos() {
     let btnEnviar = document.getElementById("enviarTurno");
     btnEnviar.addEventListener("click", (e) => {
       this.agregarTurno(e);
+      localStorage.removeItem("valueNombre");
+      localStorage.removeItem("valueNumCelular");
+      localStorage.removeItem("valueMotivo");
+      localStorage.removeItem("valueFecha");
+      localStorage.removeItem("valueHorario");
     });
   }
 }
